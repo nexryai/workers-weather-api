317ccddb0a3e8b1c82a9051639e9c83baeccb3e9
nexryai <61890205+nexryai@users.noreply.github.com>
2024-10-16 07:50:01 +0000

    WIP

--- src/index.ts
+++ src/index.ts
@@ -17,21 +17,33 @@ interface Env {
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const exists = await env.cache.get("msg")
-		if (exists) {
-			return new Response("exists");
-		}
-
 		if (!request.cf?.latitude || !request.cf?.longitude) {
-			return new Response("error");
+			return new Response("error: unable to get location");
 		}
 
-		const localtion = request.cf.latitude + request.cf.longitude
-		await env.cache.put("msg", localtion, {
+		const lat = request.cf.latitude
+		const lon = request.cf.longitude
+		const localtion = `${lat}:${lon}`
+		const cacheKey = `weather.${localtion}`
+
+		const exists = await env.cache.get(cacheKey)
+		if (exists) {
+			return new Response(exists);
+		}
+
+		const req = new Request(`https://api.met.no/weatherapi/locationforecast/2.0?lat=${lat}&lon=${lon}`, {
+			headers: {
+				"User-Agent": "Workers Wrather API (https://github.com/nexryai/workers-weather-api)",
+			},
+		})
+
+		const res = await fetch(req)
+		const resString = await res.text()
+
+		await env.cache.put(cacheKey, resString, {
 			expirationTtl: 60
 		})
 
-		const str = await env.cache.get("msg")
-		return new Response(str);
+		return new Response(resString);
 	},
 } satisfies ExportedHandler<Env>;
