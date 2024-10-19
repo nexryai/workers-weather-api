5111033201790ac46bcce1142e005164a7b8eff5
nexryai <61890205+nexryai@users.noreply.github.com>
2024-10-19 14:03:18 +0000

    Feat: use ElysiaJS for routing

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import Elysia, { Context } from "elysia";
 import { MetNorwayWeatherService } from "./services/weather/WeatherService";
 
 
@@ -20,47 +21,58 @@ interface Env {
 
 const weatherService = new(MetNorwayWeatherService)
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		let lat = request.cf?.latitude
-		let lon = request.cf?.longitude
-		let tz = request.cf?.timezone || "Asia/Tokyo"
+
+
+const app = new Elysia({ aot: false })
+
+//@ts-ignore
+app.get("/weather", async({ cache, request }) => {
+	let lat = request.cf?.latitude
+	let lon = request.cf?.longitude
+	let tz = request.cf?.timezone || "Asia/Tokyo"
+
+	if (!lat || !lon) {
+		const url = new URL(request.url)
+		lat = url.searchParams.get('lat') ?? undefined
+		lon = url.searchParams.get('lon') ?? undefined
 
 		if (!lat || !lon) {
-			const url = new URL(request.url)
-			lat = url.searchParams.get('lat') ?? undefined
-			lon = url.searchParams.get('lon') ?? undefined
-
-			if (!lat || !lon) {
-				return new Response("error: unable to get location")
-			}
+			return new Response("error: unable to get location")
 		}
+	}
 
 
-		const localtion = `${lat}:${lon}`
-		const cacheKey = `weather.${localtion}`
+	const localtion = `${lat}:${lon}`
+	const cacheKey = `weather.${localtion}`
 
-		const exists = await env.cache.get(cacheKey)
-		if (exists) {
-			return new Response(exists, {
-				headers: {
-					"Content-Type": "application/json"
-				}
-			});
-		}
-
-		const res = await weatherService.fetchWeather(lat, lon, tz)
-		const resString = JSON.stringify(res)
-
-		await env.cache.put(cacheKey, resString, {
-			// 30 min
-			expirationTtl: 1800
-		})
-
-		return new Response(resString, {
+	const exists = await cache.get(cacheKey)
+	if (exists) {
+		return new Response(exists, {
 			headers: {
 				"Content-Type": "application/json"
 			}
-		})
+		});
+	}
+
+	const res = await weatherService.fetchWeather(lat, lon, tz)
+	const resString = JSON.stringify(res)
+
+	await cache.put(cacheKey, resString, {
+		// 30 min
+		expirationTtl: 1800
+	})
+
+	return new Response(resString, {
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+})
+
+
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		app.decorate("cache", env.cache)
+		return app.fetch(request)
 	},
 } satisfies ExportedHandler<Env>;
